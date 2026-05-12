cbcce70d5db4b0d82a4574223c31cb2e5fea74f6
ganesh7228 <ganesh.r20087@gmail.com>
2026-05-12 18:21:28 +0530

    fix render dist error

--- server.ts
+++ server.ts
@@ -486,10 +486,8 @@ if (process.env.NODE_ENV !== "production") {
   });
   app.use(vite.middlewares);
 } else {
-  const distPath = path.join(process.cwd(), "dist");
-  app.use(express.static(distPath));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(distPath, "index.html"));
+  app.get("/", (req, res) => {
+    res.send("Backend is running 🚀");
   });
 }
 
